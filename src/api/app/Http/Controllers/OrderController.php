f8331a5dad007d600a4db60e208bb3cb26c7c913
viet <viet7896@gmail.com>
2023-01-28 21:58:23 +0700

    [API-13-11] Complete API

--- src/api/app/Http/Controllers/OrderController.php
+++ src/api/app/Http/Controllers/OrderController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Order;
+use App\Models\OrderItem;
 use Illuminate\Http\Request;
+use App\Http\Resources\OrderResource;
 
 class OrderController extends Controller
 {
@@ -37,5 +39,37 @@ class OrderController extends Controller
                 'message' => 'User not found'
             ], 404);
         }
+    }
+
+    public function saveToOrderItem(Request $request)
+    {
+            
+        $orderItem = OrderItem::create([
+            'order_id' => $request->orderId,
+            'pid' => $request->pid,
+            'quantity' => $request->quantity,
+            'price' => $request->price,
+        ]);
+        return response()->json([
+           $orderItem
+        ], 200);
+            
+        
+    }
+
+    public function findOrderById(Request $request)
+    {
+       
+        //select  DISTINCT * from `order`inner join order_item on `order`.orderId = order_item.orderId where `order`.orderId = "' +id 
+        $order = Order::select('order.*', 'order_item.*')
+        ->join('order_item', 'order.order_id', '=', 'order_item.order_id')
+        ->where('order.order_id', $request->id)
+        ->get();
+        $orderRes = OrderResource::collection($order);
+        return response()->json(
+             $orderRes
+        , 200);
+
+        
     }
 }
