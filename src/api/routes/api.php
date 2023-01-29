c5344f15cf7e238295444c930d42ba972e807d4c
ngtuan <nguyenngoctuan092@gmail.com>
2023-01-30 00:53:51 +0700

    [API-28] Complete

--- src/api/routes/api.php
+++ src/api/routes/api.php
@@ -38,7 +38,8 @@ Route::middleware(['api'])->group(function () {
     // /api/total30day?sid=1
     Route::get('/total30day/{query}', [StoreController::class, 'total30day']);
     Route::get('/handleOrder30day/{query}', [StoreController::class, 'handleOrder30day']);
-
+    Route::get('/totalrevenue/{query}', [StoreController::class, 'totalrevenue']);
+    
     //User
     Route::get('/user/get-info-user', [UserController::class, 'getInfoUser']);
     Route::post('/user/save-info-user', [UserController::class, 'saveInfoUser']);
