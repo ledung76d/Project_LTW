46cacaafd0fabe6e73190a48d846ae15a96af79a
dungle <ledung76d@gmail.com>
2023-01-30 20:34:03 +0700

    fixbug/API: fix param total30day

--- src/api/app/Http/Controllers/StoreController.php
+++ src/api/app/Http/Controllers/StoreController.php
@@ -154,20 +154,32 @@ class StoreController extends Controller
 
 
     
-    public function total30days($query){
-        $sid = $query->sid;
-        $total = OrderItem::selectRaw('sum(order_item.price * order_item.quantity) as total')
-            ->join('order', 'order_item.order_id', '=', 'order.order_id')
-            ->join('product', 'order_item.pid', '=', 'product.pid')
-            ->where('sid', $sid)
-            ->where('order.created_at', '>=', now()->subDays(30))
-            ->get();
-        return response()->json(
-            $total,
-            200
-        );
-    }
+    public function total30day() {
+        $user = Auth::user();
 
+    
+        if ($user) {
+            $sid = $user->id;
+            $total = OrderItem::selectRaw('sum(order_item.price * order_item.quantity) as total')
+                ->join('order', 'order_item.order_id', '=', 'order.order_id')
+                ->join('product', 'order_item.pid', '=', 'product.pid')
+                ->where('sid', $sid)
+                ->where('order.created_at', '>=', now()->subDays(30))
+                ->get();
+            return response()->json(
+                $total,
+                200
+            );
+        }else {
+            return response()->json(
+                [
+                    'status' => 'fail',
+                    'error' => 'store not found',
+                ],
+                404
+            );
+        }
+    }
 
     public function handleOrder30day($query)
     {
